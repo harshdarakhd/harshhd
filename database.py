76396f12d9cc6835a81eb632a5a60b3cd9a15cf0
harshdarakhd <108183142+harshdarakhd@users.noreply.github.com>
2022-06-30 15:25:57 +0530

    Add files via upload

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+engine=create_engine("postgresql://postgres:mylife!73@localhost/item_db",
+                     echo=True
+                     )
+
+Base=declarative_base()
+
+SessionLocal=sessionmaker(bind=engine)
